414f7309a824ef5ac7777f789bfbbde66e6c3177
Aman88600 <109413130+Aman88600@users.noreply.github.com>
2024-07-24 21:20:25 +0530

    Well the app is almost complete but its in command line so, should i make it in GUIn or not hm

--- to_do_list.py
+++ to_do_list.py
@@ -1,17 +1,53 @@
-to_do_list = []
+tasks = []
 
 def add_task():
     task = input("Enter the Task : ")
-    to_do_list.append(task)
+    tasks.append(task)
+
+def show_tasks():
+    if len(tasks) == 0:
+        print("No tasks eneted yet")
+    else :
+        count = 0
+        for i in tasks:
+            count += 1
+            print(f"{count} : {i}")
+
+def delete_task():
+    task_num = int(input("Enter task number to delete the task : "))
+    task_num -= 1
+    tasks.pop(task_num)
 
 def main():
     # Loop
     keep_going = 1
     while keep_going == 1:
-        #Add task
-        add_task()
-        print(to_do_list)
-
+        
+        # No Tasks in the list
+        if len(tasks) == 0:
+            user_choice = int(input("1 to Add task\n2 to show tasks\nEnter Input : "))
+            if user_choice == 1:
+                #Add task
+                add_task()
+            elif user_choice == 2:
+                # Show all tasks
+                show_tasks()
+            else:
+                print("Invalid Input : ")
+        # Some task or tasks in the list
+        else :
+            user_choice = int(input("1 to Add task\n2 to show tasks\n3 to delete task\nEnter Input : "))
+            if user_choice == 1:
+                #Add task
+                add_task()
+            elif user_choice == 2:
+                # Show all tasks
+                show_tasks()
+            elif user_choice == 3:
+                # Delete task
+                delete_task()
+            else:
+                print("Invalid Input : ")
         keep_going = int(input("Press 1 to keep going any other key to stop : "))
 if __name__ == "__main__":
     main()
