6ae61e1293038cfc3657f924988813e791baefa5
Aman88600 <109413130+Aman88600@users.noreply.github.com>
2024-07-27 11:26:30 +0530

    Made the password generator

--- password_generator.py
+++ password_generator.py
@@ -1,35 +1,62 @@
 import customtkinter
 
-customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
-customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
 
 app = customtkinter.CTk()  # create CTk window like you do with the Tk window
 app.geometry("400x240")
 app.title("Password Generator")
 
 
-# # Number 1
-# number_1_entry = customtkinter.CTkEntry(master=app, placeholder_text="Enter Number 1")
-# number_1_entry.place(relx=0.5, rely=0.1, anchor=customtkinter.CENTER)
+# PyPassword Generator
+import random
 
+# Generator function
+def generator(number_of_something, string_of_something):
+    password = []
+    letters = string_of_something
+    for i in range(0, number_of_something):
+        letters = list(letters)
+        num = random.randint(0, len(letters) - 1)
+        password.append(letters[num])
+    # letters.pop(num)
+    password_str = "".join(password)
+    return password_str
 
-# # Number 2
-# number_2_entry = customtkinter.CTkEntry(master=app, placeholder_text="Enter Number 2")
-# number_2_entry.place(relx=0.5, rely=0.3, anchor=customtkinter.CENTER)
+# Shuffle Function
+def shuffle(password_string):
+    password_string = list(password_string)
+    num1 = random.randint(0, len(password_string) - 1)
+    num2 = random.randint(0, len(password_string) - 1)
+    # Swaping
+    temp = password_string[num1]
+    password_string[num1] = password_string[num2]
+    password_string[num2] = temp
+    password_string = "".join(password_string)
+    return password_string
 
-# # Operation
-# operation = customtkinter.CTkEntry(master=app, placeholder_text="Enter Operation")
-# operation.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
-
-# # Output
-# output = customtkinter.CTkLabel(master=app, text="Output")
-# output.place(relx=0.5, rely=0.7, anchor=customtkinter.CENTER)
-
+letters_input = customtkinter.CTkEntry(master=app, placeholder_text="How many letters")
+letters_input.place(relx=0.5, rely=0.1, anchor=customtkinter.CENTER)
+symbols_input = customtkinter.CTkEntry(master=app, placeholder_text="How many symbols")
+symbols_input.place(relx=0.5, rely=0.3, anchor=customtkinter.CENTER)
+numbers_input = customtkinter.CTkEntry(master=app, placeholder_text="How many numbers")
+numbers_input.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+generated_password = customtkinter.CTkLabel(master=app, text="Password")
+generated_password.place(relx=0.5, rely=0.7, anchor=customtkinter.CENTER)
 
 def button_function():
-    print("Button Clicked")
+    letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    number_of_letters = int(letters_input.get())
+    letters_str = generator(number_of_letters, letters)
+    numbers = "0123456789"
+    number_of_numbers = int(numbers_input.get())
+    numbers_str = generator(number_of_numbers, numbers)
+    symbols = "!@#$%^&*()[]<>?/+-"
+    number_of_symbols = int(symbols_input.get())
+    symbols_str = generator(number_of_symbols, symbols)
 
-# Use CTkButton instead of tkinter Button
+    generated_password.configure(text = str(letters_str + numbers_str + symbols_str))
+
 button = customtkinter.CTkButton(master=app, text="Generate", command=button_function)
 button.place(relx=0.5, rely=0.9, anchor=customtkinter.CENTER)
 
